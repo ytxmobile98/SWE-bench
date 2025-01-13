187d6eba8b1583cedf24a09cf9f8441d298239af
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-01-13 10:07:05 +0800

    style: updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python -m swebench.harness.run_evaluation     --predictions_path gold     --max_workers 16     --instance_ids sympy__sympy-20590     --run_id validate-gold
+python -m swebench.harness.run_evaluation \
+    --predictions_path gold \
+    --max_workers 16 \
+    --instance_ids sympy__sympy-20590 \
+    --run_id validate-gold
