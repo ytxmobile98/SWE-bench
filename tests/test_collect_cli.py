124897d265035861b82f18e9999e8f66024dca45
John Yang <byjohnyang@gmail.com>
2025-05-06 19:32:13 +0000

    Add test for PR #358

--- tests/test_collect_cli.py
+++ tests/test_collect_cli.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 
 
@@ -39,3 +40,24 @@ def test_collect_ds(tmp_path):
     print(result.stdout)
     print(result.stderr)
     assert result.returncode == 0
+
+
+def test_collect_get_issues(tmp_path):
+    # python print_pulls.py lowRISC/opentitan output_pr_26371.json --pull_number 26371
+    cmd = [
+        "python",
+        "-m",
+        "swebench.collect.print_pulls",
+        "lowRISC/opentitan",
+        str(tmp_path / "output_pr_26371.json"),
+        "--pull_number",
+        "26371",
+    ]
+    print(" ".join(cmd))
+    result = subprocess.run(cmd, capture_output=True)
+    print(result.stdout)
+    print(result.stderr)
+    assert result.returncode == 0
+    data = json.loads((tmp_path / "output_pr_26371.json").read_text())
+    assert len(data["resolved_issues"]) == 2
+    assert sorted(data["resolved_issues"]) == ["26194", "26230"]
